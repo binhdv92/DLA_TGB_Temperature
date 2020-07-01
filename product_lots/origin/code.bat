148d84e66bae06d38df053beff98023d3efd92e4
binhdv92 <van.binh.duong@intel.com>
2020-07-01 22:01:17 +0700

    first commit

---
+++ product_lots/origin/code.bat
@@ -0,0 +1,18 @@
+@echo off
+
+mkdir DIA001
+mkdir DIA002
+mkdir DIA003
+mkdir DIA004
+mkdir DIA005
+mkdir DIA006
+mkdir DIA007
+mkdir DIA008
+mkdir DIA009
+mkdir DIA010
+mkdir DIA011
+mkdir DIA012
+mkdir DIA013
+mkdir DIA014
+
+SET /P DUMMY=Press any key to continue
